f9694522bc4e321ec16a1068096c682ae5a592f1
heshima243 <heshimajulien.official@gmail.com>
2024-07-24 14:13:18 +0200

    Fix CORS and configuration issues

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const path = require('path');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({ origin: 'https://crush-rust.vercel.app' }));
 
 const mongoURI = "mongodb+srv://heshimajulienofficial:gZo66bAOKJBetFSQ@localisation.st4rgvh.mongodb.net/?retryWrites=true&w=majority&appName=localisation";
-
 const port = process.env.PORT || 3000;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,10 +21,6 @@ const locationSchema = new mongoose.Schema({
     longitude: Number,
     timestamp: Date
 });
-
-// Configurer CORS
-app.use(cors({ origin: 'https://crush-rust.vercel.app' }));
-
 
 const Location = mongoose.model('Location', locationSchema);
 
@@ -45,44 +41,5 @@ app.post('/location', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Server is running on port ',port);
+    console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-//-----------------------------------------------------------------------
-
-// const axios = require('axios');
-
-// const API_KEY = '0d91477bf1894b3f9ba9d868227882c1'; // Remplacez par votre clé API
-// const latitude = -11.6588544;
-// const longitude = 27.4890752;
-
-// const getGeolocationInfo = async (lat, lon) => {
-//     const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${API_KEY}`;
-    
-//     try {
-//         const response = await axios.get(url);
-//         const results = response.data.results;
-//         if (results.length > 0) {
-//             const address = results[0].formatted;
-//             const components = results[0].components; // Pour détails plus fins comme les adresses de rue
-//             return {
-//                 address,
-//                 components
-//             };
-//         } else {
-//             return 'No results found';
-//         }
-//     } catch (error) {
-//         console.error('Error fetching geolocation data:', error);
-//         return 'Error fetching geolocation data';
-//     }
-// };
-
-// getGeolocationInfo(latitude, longitude).then(data => {
-//     console.log('Address:', data.address);
-//     console.log('Components:', data.components);
-// });
-
